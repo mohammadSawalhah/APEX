e8118d67753a97d333d8bd68fbc89fa061108707
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:38:50 +0300

    feat:db-export Export TRIGGER TR_SELLER_ASSIGNED_FILES_DETAILS

---
+++ triggers/tr_seller_assigned_files_details.trg.sql
@@ -0,0 +1,21 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_SELLER_ASSIGNED_FILES_DETAILS" BEFORE
+    INSERT OR UPDATE OR DELETE ON SELLER_ASSIGNED_FILES_DETAILS    
+    FOR EACH ROW 
+BEGIN    
+    IF inserting THEN    
+        IF :new.ID IS NULL THEN    
+            :new.ID := SELLER_ASSIGNED_FILES_DETAILS_SEQ.nextval;    
+        END IF;
+        :new.created_date := SYSDATE;    
+        :new.created_by := nvl(v('APP_USER'), user);    
+        :new.modified_date := SYSDATE;    
+        :new.modified_by := nvl(v('APP_USER'), user);
+    ELSIF updating then
+        :new.modified_date := SYSDATE;    
+        :new.modified_by := nvl(v('APP_USER'), user);
+    ENd IF;
+END;
+
+/
+ALTER TRIGGER "MOBASHER"."TR_SELLER_ASSIGNED_FILES_DETAILS" ENABLE;
