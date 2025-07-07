d9b97a5016c54a53d29ba932d09ab0f1a5900a2b
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:38:16 +0300

    feat:db-export Export TRIGGER TR_MOBASHER_DEPARTMENTS

---
+++ triggers/tr_mobasher_departments.trg.sql
@@ -0,0 +1,22 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_MOBASHER_DEPARTMENTS" BEFORE     
+    INSERT OR UPDATE OR DELETE ON MOBASHER_DEPARTMENTS     
+    FOR EACH ROW  
+BEGIN     
+    IF inserting THEN     
+        IF :new.ID IS NULL THEN     
+            :new.ID := MOBASHER_DEPARTMENTS_SEQ.nextval;     
+        END IF; 
+        :new.created_date := SYSDATE;     
+        :new.created_by := nvl(v('APP_USER'), user);     
+        :new.modified_date := SYSDATE;     
+        :new.modified_by := nvl(v('APP_USER'), user);   
+    ELSIF updating THEN     
+        :new.modified_date := SYSDATE;     
+        :new.modified_by := nvl(v('APP_USER'), user);   
+    ENd IF; 
+END;
+
+
+/
+ALTER TRIGGER "MOBASHER"."TR_MOBASHER_DEPARTMENTS" ENABLE;
