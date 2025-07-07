2e4db851c73bfe4b5b2d449238f651cd03facfd2
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:37:44 +0300

    feat:db-export Export TRIGGER TR_INDIVIDUAL_PORTABLE_REQUESTS

---
+++ triggers/tr_individual_portable_requests.trg.sql
@@ -0,0 +1,22 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_INDIVIDUAL_PORTABLE_REQUESTS" 
+  BEFORE INSERT OR UPDATE 
+  ON  INDIVIDUAL_PORTABLE_REQUESTS
+  FOR EACH ROW 
+    BEGIN 
+        IF INSERTING THEN 
+          IF :new.ID IS NULL THEN    
+               :new.ID := INDIVIDUAL_PORTABLE_REQUESTS_SEQ.nextval;    
+           END IF;
+           :new.CREATED_DATE := SYSDATE; 
+           :new.CREATED_BY := nvl(v('APP_USER'), user);
+        END IF; 
+
+        IF updating THEN    
+         :new.MODIFIED_DATE := SYSDATE;    
+         :new.MODIFIED_BY := nvl(v('APP_USER'), user);  
+        END IF;
+    END;
+
+/
+ALTER TRIGGER "MOBASHER"."TR_INDIVIDUAL_PORTABLE_REQUESTS" ENABLE;
