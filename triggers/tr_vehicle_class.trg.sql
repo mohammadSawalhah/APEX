59bbd52c2c1a8804cb793697bc7b68ed79d55e4e
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:39:25 +0300

    feat:db-export Export TRIGGER TR_VEHICLE_CLASS

---
+++ triggers/tr_vehicle_class.trg.sql
@@ -0,0 +1,20 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_VEHICLE_CLASS" BEFORE
+    INSERT OR UPDATE OR DELETE ON VEHICLE_CLASS    
+    FOR EACH ROW 
+BEGIN    
+    IF inserting THEN    
+        IF :new.ID IS NULL THEN    
+            :new.ID := VEHICLE_CLASS_SEQ.nextval;    
+        END IF;
+        :new.created_date := SYSDATE;    
+        :new.created_by := nvl(v('APP_USER'), user);
+    ELSIF updating THEN    
+        :new.modified_date := SYSDATE;    
+        :new.modified_by := nvl(v('APP_USER'), user);
+    ENd IF;
+END;
+
+
+/
+ALTER TRIGGER "MOBASHER"."TR_VEHICLE_CLASS" ENABLE;
