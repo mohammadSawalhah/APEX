da2f9fb6c6fe47579064367d202dffeb28b0bea2
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:37:57 +0300

    feat:db-export Export TRIGGER TR_INDIVIUAL_REALESTATE_REQUESTS

---
+++ triggers/tr_indiviual_realestate_requests.trg.sql
@@ -0,0 +1,23 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_INDIVIUAL_REALESTATE_REQUESTS" 
+  BEFORE INSERT OR UPDATE 
+  ON "INDIVIDUAL_REALESTATE_REQUESTS"
+  FOR EACH ROW 
+    BEGIN 
+        IF INSERTING THEN 
+          IF :new.ID IS NULL THEN    
+               :new.ID := INDIVIUAL_REALESTATE_REQUESTS_SEQ.nextval;    
+           END IF;
+           :new.CREATED_DATE := SYSDATE; 
+           :new.CREATED_BY := nvl(v('APP_USER'), user);
+        END IF; 
+
+        IF updating THEN    
+         :new.MODIFIED_DATE := SYSDATE;    
+         :new.MODIFIED_BY := nvl(v('APP_USER'), user);  
+        END IF;
+    END;
+
+
+/
+ALTER TRIGGER "MOBASHER"."TR_INDIVIUAL_REALESTATE_REQUESTS" ENABLE;
