807aad2004de7853dbb30022126618d4b21b15f4
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:37:52 +0300

    feat:db-export Export TRIGGER TR_INDIVIDUAL_REALESTATE_DECLINED_REQUESTS

---
+++ triggers/tr_individual_realestate_declined_requests.trg.sql
@@ -0,0 +1,21 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_INDIVIDUAL_REALESTATE_DECLINED_REQUESTS" BEFORE 
+  INSERT OR UPDATE ON INDIVIDUAL_REALESTATE_DECLINED_REQUESTS
+  FOR EACH ROW
+BEGIN 
+    IF INSERTING THEN    
+        IF :new.ID IS NULL THEN    
+           :new.ID := INDIVIDUAL_REALESTATE_DECLINED_REQUESTS_SEQ.nextval;    
+        END IF;
+        :new.created_date := SYSDATE;    
+        :new.created_by := nvl(v('APP_USER'), user);    
+        :new.modified_date := SYSDATE;    
+        :new.modified_by := nvl(v('APP_USER'), user);  
+    ELSIF UPDATING THEN
+        :new.modified_date := SYSDATE;
+        :new.modified_by := nvl(v('APP_USER'), user);
+    ENd IF;
+END TR_INDIVIDUAL_REALESTATE_DECLINED_REQUESTS;
+
+/
+ALTER TRIGGER "MOBASHER"."TR_INDIVIDUAL_REALESTATE_DECLINED_REQUESTS" ENABLE;
