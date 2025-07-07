795f2e8a40b851d67803d2efe2ee5477fe0af32b
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:37:51 +0300

    feat:db-export Export TRIGGER TR_INDIVIDUAL_REALESTATE_CHAT

---
+++ triggers/tr_individual_realestate_chat.trg.sql
@@ -0,0 +1,24 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_INDIVIDUAL_REALESTATE_CHAT" 
+  BEFORE INSERT OR UPDATE 
+  ON INDIVIDUAL_REALESTATE_CHAT 
+  FOR EACH ROW 
+    BEGIN 
+        IF INSERTING THEN 
+          IF :new.ID IS NULL THEN    
+             :new.ID := INDIVIDUAL_REALESTATE_CHAT_SEQ.nextval;    
+          END IF;
+           IF :new.CREATED_BY IS NULL THEN
+          :new.CREATED_BY := nvl(v('APP_USER'), user);
+           END IF;
+          :new.CREATED_DATE := SYSDATE; 
+        END IF; 
+
+        IF UPDATING THEN    
+         :new.MODIFIED_DATE := SYSDATE;    
+         :new.MODIFIED_BY := nvl(v('APP_USER'), user);  
+        END IF;
+    END;
+
+/
+ALTER TRIGGER "MOBASHER"."TR_INDIVIDUAL_REALESTATE_CHAT" ENABLE;
