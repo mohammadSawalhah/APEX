faede164e5ffe93877a7be6255a9f2ab0098c78d
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:37:23 +0300

    feat:db-export Export TRIGGER TR_CURRENCY

---
+++ triggers/tr_currency.trg.sql
@@ -0,0 +1,19 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_CURRENCY" BEFORE
+    INSERT OR UPDATE OR DELETE ON CURRENCY    
+    FOR EACH ROW 
+BEGIN    
+    IF inserting THEN    
+        IF :new.ID IS NULL THEN    
+            :new.ID := CURRENCY_SEQ.nextval;    
+        END IF;
+        :new.created_date := SYSDATE;    
+        :new.created_by := nvl(v('APP_USER'), user);    
+        :new.modified_date := SYSDATE;    
+        :new.modified_by := nvl(v('APP_USER'), user);  
+    ENd IF;
+END;
+
+
+/
+ALTER TRIGGER "MOBASHER"."TR_CURRENCY" ENABLE;
