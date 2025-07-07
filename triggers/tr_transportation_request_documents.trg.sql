e93280bb4e040c93dd4c4ddaed9aaa1cf64d4879
Mohammad Sawalhah <mohammad.sawalhah@mobasher.sa>
2025-07-07 15:39:18 +0300

    feat:db-export Export TRIGGER TR_TRANSPORTATION_REQUEST_DOCUMENTS

---
+++ triggers/tr_transportation_request_documents.trg.sql
@@ -0,0 +1,24 @@
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "MOBASHER"."TR_TRANSPORTATION_REQUEST_DOCUMENTS" 
+  BEFORE INSERT OR UPDATE  
+  ON  TRANSPORTATION_REQUEST_DOCUMENTS 
+  FOR EACH ROW  
+    BEGIN  
+        IF INSERTING THEN  
+          IF :new.ID IS NULL THEN     
+               :new.ID := TRANSPORTATION_REQUEST_DOCUMENTS_SEQ.nextval;     
+           END IF; 
+           :new.CREATED_DATE := SYSDATE;  
+           :new.CREATED_BY := nvl(v('APP_USER'), user); 
+        END IF;  
+ 
+        IF updating THEN     
+         :new.MODIFIED_DATE := SYSDATE;     
+         :new.MODIFIED_BY := nvl(v('APP_USER'), user);   
+        END IF; 
+    END;
+
+
+
+/
+ALTER TRIGGER "MOBASHER"."TR_TRANSPORTATION_REQUEST_DOCUMENTS" ENABLE;
